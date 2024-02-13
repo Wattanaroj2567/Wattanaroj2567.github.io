974374064cbe5731e181bd997a4008b9f78c96e0
Tawan <159878664+Wattanaroj2567@users.noreply.github.com>
2024-02-13 17:01:37 +0700

    Add files via upload

---
+++ app01.py
@@ -0,0 +1,4 @@
+# steamlit run streamlit/app01.py
+import streamlit as st  
+
+st.header('My OOP Notebook')
---
+++ app02.py
@@ -0,0 +1,9 @@
+# steamlit run streamlit/app02.py
+import streamlit as st  
+st.sidebar.text('sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+st.image('https://picsum.photos/500/200')
+st.text_input("What's your name")
