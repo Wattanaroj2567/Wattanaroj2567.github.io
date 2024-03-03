df99710214a6adb0d7fd3b20a05fc6c69055c879
Wattanaroj Butdee <159878664+Wattanaroj2567@users.noreply.github.com>
2024-03-03 19:16:59 +0700

    Update app02.py

--- streamlit/app02.py
+++ streamlit/app02.py
@@ -3,6 +3,6 @@ import streamlit
 h = streamlit.header('My Web Site on Diffusion')
 s = streamlit.subheader('เว็บไซต์ส่วนตัวของผม')
 p = streamlit.write('เว็บไซต์แลกมาด้วยหยาดเหงื่อและความอดทน')
-banner = streamlit.image('htts://picsum.photos/800/250')
+banner = streamlit.image('https://picsum.photos/800/250')
 b = streamlit.button('Click me ')
 
